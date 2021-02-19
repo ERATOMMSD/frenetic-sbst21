1725665ce00ff4912d58e1d03672a6e1fa3f727e
ezecastellano <ezecastellano@users.noreply.github.com>
2021-02-19 18:30:57 +0900

    Update frenet.py

--- src/utils/frenet.py
+++ src/utils/frenet.py
@@ -2,6 +2,10 @@ import numpy as np
 
 # TODO: needs improvement in choosing correct number of x, y, theta
 def frenet_to_cartesian(x0, y0, theta0, ss, kappas):
+    """
+     Numerical integration with trapezoidal rule to transform curvature samples 
+     given in Frenet frame to points in Cartesian frame
+    """
     xs = np.zeros(ss.shape[0])
     ys = np.zeros(ss.shape[0])
     thetas = np.zeros(ss.shape[0])
